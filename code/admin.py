faab72c30cabc867e1ea13f288c3c124b61a97f6
IPSSandhu <ishan3121@uvic.ca>
2024-11-12 22:26:13 -0800

    Update admin.py

--- code/admin.py
+++ code/admin.py
@@ -63,6 +63,9 @@ class SystemAdministrator(SystemAdmin):
         print("Checking for software updates....")
         time.sleep(2)
         print("System software is already updated.")
+        time.sleep(2)
+        input("\nPress Enter to go back to the Dashboard...")
+
 
     def perform_security_check(self):
         print("Starting security check...")
@@ -98,6 +101,8 @@ class SystemAdministrator(SystemAdmin):
         print("Data integrity check passed successfully.")
         time.sleep(2)
         print("System audit completed successfully.")
+        time.sleep(2)
+        input("\nPress Enter to go back to the Dashboard...")
 
     def __str__(self):
         return f"System Administrator: {self.username}"
